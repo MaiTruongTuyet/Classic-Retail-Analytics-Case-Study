750acd35b39a9415007e510a081dd96cff90c1df
Tuyet Mai Truong <105296674+MaiTruongTuyet@users.noreply.github.com>
2025-06-11 14:43:11 +0700

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,36 +1,36 @@
--- Revenue Trends and Product-Level Performance
--- 1. Average Amount by Country
-Select c.country, round(avg(s.amount),2) as "average amount"
-from customers c
-join payments s
-on c.customerNumber = s.customerNumber
-group by c.country
--- 2. Sale Amount by Product Line
-Select p.productLine, round(Sum(o.priceEach*quantityOrdered),2) as "Sale Amount"
-from orderdetails o
-join products p
-on o.productCode = p.productCode
-group by p.productLine
--- 3. top 10 best selling product by quantity
-Select p.productName as "product", sum(o.quantityOrdered) as "quantity"
-from orderdetails o
-join products p
-on o.productCode = p.productCode
-group by p.productName
-order by quantity desc
-limit 10
--- 4. Average Number of Orders per Customer
+-- **REVENUE TRENDS AND PRODUCT-LEVEL PERFORMANCE**
+
+-- **1. AVERAGE AMOUNT BY COUNTRY**
+SELECT c.country, ROUND(AVG(s.amount), 2) AS "average amount"
+FROM customers c
+JOIN payments s ON c.customerNumber = s.customerNumber
+GROUP BY c.country;
+
+-- **2. SALE AMOUNT BY PRODUCT LINE**
+SELECT p.productLine, ROUND(SUM(o.priceEach * quantityOrdered), 2) AS "Sale Amount"
+FROM orderdetails o
+JOIN products p ON o.productCode = p.productCode
+GROUP BY p.productLine;
+
+-- **3. TOP 10 BEST SELLING PRODUCT BY QUANTITY**
+SELECT p.productName AS "product", SUM(o.quantityOrdered) AS "quantity"
+FROM orderdetails o
+JOIN products p ON o.productCode = p.productCode
+GROUP BY p.productName
+ORDER BY quantity DESC
+LIMIT 10;
+
+-- **4. AVERAGE NUMBER OF ORDERS PER CUSTOMER**
 SELECT 
     COUNT(o.orderNumber) * 1.0 / COUNT(DISTINCT c.customerNumber) AS avg_orders_per_customer
 FROM orders o
-JOIN customers c 
-ON o.customerNumber = c.customerNumber;
+JOIN customers c ON o.customerNumber = c.customerNumber;
 
--- Operation
--- 5. On time shipment percentage
+-- **OPERATION**
+
+-- **5. ON TIME SHIPMENT PERCENTAGE**
 WITH ontime_order AS (
-    SELECT 
-        orderNumber
+    SELECT orderNumber
     FROM orders
     WHERE shippedDate <= requiredDate
 ),
@@ -39,8 +39,7 @@ cte AS (
         COUNT(DISTINCT o.orderNumber) AS total_order, 
         COUNT(lo.orderNumber) AS on_time_order
     FROM orders o
-    LEFT JOIN ontime_order lo
-        ON o.orderNumber = lo.orderNumber
+    LEFT JOIN ontime_order lo ON o.orderNumber = lo.orderNumber
     WHERE o.status = 'Shipped'
 )
 SELECT 
@@ -48,7 +47,8 @@ SELECT
     on_time_order,
     ROUND(1.0 * on_time_order / total_order * 100, 2) AS on_time_rate
 FROM cte;
---c2
+
+-- **CÁCH 2**
 SELECT 
     COUNT(DISTINCT orderNumber) AS total_order,
     COUNT(DISTINCT CASE WHEN shippedDate <= requiredDate THEN orderNumber END) AS on_time_order,
@@ -59,19 +59,24 @@ SELECT
     ) AS on_time_rate
 FROM orders
 WHERE status = 'Shipped';
--- 6. Late Shipments Identify
+
+-- **6. LATE SHIPMENTS IDENTIFY**
 SELECT *
 FROM orders 
-WHERE shippedDate > requiredDate
--- Customer Behaviour
--- 8. based on their total purchase amount into "High Value," "Medium Value," and "Low Value" categories.
-with cte as (
-    select customerNumber, round(sum(amount),2) as "total_spent"
-    from payments
-    group by customerNumber),
+WHERE shippedDate > requiredDate;
+
+-- **CUSTOMER BEHAVIOUR**
+
+-- **8. CUSTOMER SEGMENTATION BY TOTAL PURCHASE AMOUNT**
+WITH cte AS (
+    SELECT customerNumber, ROUND(SUM(amount), 2) AS total_spent
+    FROM payments
+    GROUP BY customerNumber
+),
 customer_segments AS (
     SELECT 
-        customerNumber, total_spent,
+        customerNumber, 
+        total_spent,
         CASE 
             WHEN total_spent >= 100000 THEN 'High Value'
             WHEN total_spent >= 50000 THEN 'Medium Value'
@@ -79,12 +84,12 @@ customer_segments AS (
         END AS segment
     FROM cte
 )
-Select c.customerName, cs.segment, cs.total_spent
-from customers c
-left join customer_segments cs
-on c.customerNumber = cs.customerNumber
-order by c.customerName
--- cách 2
+SELECT c.customerName, cs.segment, cs.total_spent
+FROM customers c
+LEFT JOIN customer_segments cs ON c.customerNumber = cs.customerNumber
+ORDER BY c.customerName;
+
+-- **CÁCH 2**
 WITH customer_payment AS (
     SELECT customerNumber, amount
     FROM payments
@@ -100,22 +105,24 @@ SELECT DISTINCT
 FROM customers c
 LEFT JOIN customer_payment cp ON c.customerNumber = cp.customerNumber
 ORDER BY c.customerName;
--- 9. Frequently Co-Purchased Products
--- Dùng self join lấy các cặp sản phẩm được mua cùng nhau
-select
-    case
-    when od1.productCode < od2.productCode then od1.productCode
-    else od2.productCode end as productid_1,
-    case
-    when od1.productCode > od2.productCode then od1.productCode
-    else od2.productCode end as productid_2,    
-    count(*) as "time"
-from orderdetails od1
-join orderdetails od2
-on od1.orderNumber = od2.orderNumber
-and od1.productCode < od2.productCode
-group by productid_1, productid_2
-order by time desc
-select *
-from orderdetails
-order by productCode desc
+
+-- **9. FREQUENTLY CO-PURCHASED PRODUCTS**
+SELECT
+    CASE
+        WHEN od1.productCode < od2.productCode THEN od1.productCode
+        ELSE od2.productCode
+    END AS productid_1,
+    CASE
+        WHEN od1.productCode > od2.productCode THEN od1.productCode
+        ELSE od2.productCode
+    END AS productid_2,    
+    COUNT(*) AS "time"
+FROM orderdetails od1
+JOIN orderdetails od2 ON od1.orderNumber = od2.orderNumber
+    AND od1.productCode < od2.productCode
+GROUP BY productid_1, productid_2
+ORDER BY time DESC;
+
+SELECT *
+FROM orderdetails
+ORDER BY productCode DESC;
